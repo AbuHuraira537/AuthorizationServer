30c90c20e269a8344965f965bf719d6ec6321b25
huraira <git@techno-soft.com>
2022-04-08 16:34:59 +0500

    Add project files.

---
+++ Authorization-Server/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Authorization_Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
